17bf335d5c6592180f53d82e6fe057995e7177ae
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2021-08-12 02:32:56 +0800

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+    }
+
+
+
+}
